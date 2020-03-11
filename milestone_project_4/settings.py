8b316b180d38311ed5c3d2b78309db518e0c8390
Darren Mc Gill <darrenmcgill@live.co.uk>
2020-03-11 19:27:07 +0000

    added files

--- milestone_project_4/settings.py
+++ milestone_project_4/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'blog')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
