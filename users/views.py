966befbbe3cc3fdef9e6e60b1598884a68770fca
Darren Mc Gill <darrenmcgill@live.co.uk>
2020-03-26 09:19:22 +0000

    update views.py

--- users/views.py
+++ users/views.py
@@ -21,14 +21,15 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-
-            messages.success(request, f'Your account has been Updated!')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
@@ -37,4 +38,5 @@ def profile(request):
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
