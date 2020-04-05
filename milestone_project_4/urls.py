ef8b486c58fe9325b1610e352969a16d0b2d133d
Darren Mc Gill <darrenmcgill@live.co.uk>
2020-04-05 14:53:06 +0100

    update settings

--- milestone_project_4/urls.py
+++ milestone_project_4/urls.py
@@ -21,15 +21,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from cart import views as view_cart_views
-from store.views import all_stores
 from users import views as user_views
+from store import views as all_stores_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('store/', all_stores, name='stores'),
+    path('store/', all_stores_views.all_stores, name='store'),
     path('cart/', view_cart_views.view_cart, name='view_cart'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
