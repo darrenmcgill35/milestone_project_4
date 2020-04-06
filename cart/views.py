402d45af9338184d5c9762a6d5c9ffed32a4bcff
Darren Mc Gill <darrenmcgill@live.co.uk>
2020-04-06 18:30:33 +0100

    update checkout

--- cart/views.py
+++ cart/views.py
@@ -1,30 +1,34 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 
 # Create your views here.
+from store.models import Store
+
+
 def view_cart(request):
     return render(request, "cart/cart.html")
 
 
-def add_to_cart(request, id):
+def add_to_cart(request, store_id):
+
     quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
-    if id in cart:
-        cart[id] = int(cart[id]) + quantity
+    if store_id in cart:
+        cart[store_id] = int(cart[store_id]) + quantity
     else:
-        cart[id] = cart.get(id, quantity)
+        cart[store_id] = cart.get(store_id, quantity)
 
     request.session['cart'] = cart
     return redirect(reverse('store'))
 
 
-def adjust_cart(request, id):
+def adjust_cart(request, store_id):
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
-        cart[id] = quantity
+        cart[store_id] = quantity
     else:
         cart.pop(id)
 
