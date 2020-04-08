9a84a48cabee6ba0a672c19e54162187723bc9c6
Darren Mc Gill <darrenmcgill@live.co.uk>
2020-04-08 16:22:27 +0100

    update settings

--- store/views.py
+++ store/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from blog.models import Post
 from .models import Store
 
 
 @login_required
 def all_stores(request):
     stores = Store.objects.all()
+
     return render(request, 'store/store.html', {"stores": stores})
 
+#
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'store/store.html', context)
