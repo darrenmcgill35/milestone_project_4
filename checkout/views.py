51d1352ad034de0a3ff6dd7e3df6d2142d5e5532
Darren Mc Gill <darrenmcgill@live.co.uk>
2020-04-16 19:28:50 +0100

    update settings.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 from blog.models import Post
 from .forms import MakePaymentForm, OrderForm
 from .models import OrderLineItem
